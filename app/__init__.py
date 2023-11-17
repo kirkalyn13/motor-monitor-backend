5816da0d013cb4c265ffd35557d2580ce2f79847
Kirk Santos <kirkalyn13@gmail.com>
2023-11-17 08:18:05 +0800

    feat: config cors

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,17 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app) 
+
+# Define CORS options
+cors_config = {
+    "origins": ["https://motor-monitor-frontend.vercel.app", "http://localhost:3000"], 
+    "methods": ["GET", "POST", "PUT", "DELETE"], 
+    "allow_headers": ["Content-Type"],
+    "supports_credentials": True,
+    "max_age": 120,
+}
+
+CORS(app, resources={r"/api/*": cors_config}) 
 
 # Import controllers here: 
 from controllers import metrics_controller
